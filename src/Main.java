78576aa87f52b64e6087f54cc9f8b58ddafbe886
dl57934 <dl57934@pukyong.ac.kr>
2019-04-26 17:36:33 +0900

    complete Algorithm Test

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,60 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     static int WORD = 0;
+    static int SAME = 0;
     static int CLAS = 1;
     static int DESCRIPTION = 2;
     static String FILE_PATH = "/Users/leesanghoon/Desktop/학교생활/알고리즘/6주차/pa-05-dl57934/shuffled_dict.txt";
+    static String DELETE_ALL_FILE_PATH =  "/Users/leesanghoon/Desktop/학교생활/알고리즘/6주차/pa-05-dl57934/to_be_deleted_words.txt";
+    static Queue<BST.Node> qu = new LinkedList<>();
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws IOException {
         BST bst = new BST();
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+        setTree(bst);
+
+
+        while(true) {
+            String command = bfr.readLine();
+            System.out.println(command);
+            if (command != null) {
+                if (compareWord(command, "size") == SAME) {
+                    System.out.println(bst.getSize());
+                } else if (compareWord(command, "find") == SAME) {
+                    String findWord = bfr.readLine();
+                    bst.search(bst.root, findWord);
+                } else if (compareWord(command, "add") == SAME) {
+                    System.out.print("word: ");
+                    String words = bfr.readLine();
+                    System.out.print("class: ");
+                    String clas = bfr.readLine();
+                    System.out.print("meaning: ");
+                    String meaning = bfr.readLine();
+                    bst.appendNode(words, clas, meaning);
+                } else if (compareWord(command, "deleteAll") == SAME) {
+                   BufferedReader bfr2 = getDeleteWords();
+                    String line;
+                    while((line = bfr2.readLine()) != null){
+                        System.out.println(line);
+                        bst.deleteKey(line);
+                    }
+                } else if (compareWord(command, "delete") == SAME) {
+                    String deleteWord = bfr.readLine();
+                    bst.deleteKey(deleteWord);
+                }
+            }
+        }
+
+    }
+
+    public static BufferedReader getDeleteWords() throws FileNotFoundException{
+        return new BufferedReader(new FileReader(DELETE_ALL_FILE_PATH));
+    }
+
+    public static BST setTree(BST bst) throws FileNotFoundException, IOException {
         String line;
         BufferedReader bfr = new BufferedReader(new FileReader(FILE_PATH));
 
@@ -18,11 +62,23 @@ public class Main {
             String[] array = new String[10];
             copyArray(array, splitLine(line));
             bst.appendNode(array[WORD], array[CLAS], array[DESCRIPTION]);
-
         }
 
-//        bst.initRoot()
+        return bst;
 
+    }
+
+    public static void levelOrder(BST.Node bst){
+        qu.offer(bst);
+        while(!qu.isEmpty()){
+            BST.Node node= qu.poll();
+            System.out.println(node.word);
+
+            if(node.left !=null)
+                qu.offer(node.left);
+            if(node.right != null)
+                qu.offer(node.right);
+        }
     }
 
     static String[] splitLine(String line){
@@ -30,13 +86,13 @@ public class Main {
         String[] classDescription = word[1].split("\\)", 0);
        try{
            return new String[]{
-                   word[0],
+                   word[0].trim(),
                    classDescription[0],
                    classDescription[1]
            };
        }catch (Exception e) {
            return new String[]{
-                   word[0],
+                   word[0].trim(),
                    classDescription[0],
                    ""
            };
@@ -47,12 +103,16 @@ public class Main {
         System.arraycopy(array, 0, copiedArray, 0, array.length);
     }
 
+    public static int compareWord(String w1, String w2){
+        return w1.compareTo(w2);
+    }
+
 }
 
 
 class BST{
-    int SAME = 0;
-
+    private int SAME = 0;
+    private int size = 0;
     class Node{
         Node left;
         Node right;
@@ -68,15 +128,18 @@ class BST{
             this.right = null;
         }
     }
-
-    Node root = null;
+    Node root;
+    public BST(){
+        root = null;
+    }
 
     public void appendNode(String word, String clas, String description){
-        addNode(root, word, clas, description);
+        root = addNode(root, word, clas, description);
     }
 
     public Node addNode(Node root, String word, String clas, String description){
         if(root == null) {
+            size+=1;
             root = new Node(word, clas, description);
             return root;
         }
@@ -88,39 +151,61 @@ class BST{
         return root;
     }
 
+    public int getSize(){
+        return size;
+    }
+
+
     public Node search(Node root, String word){
-        if(root ==null || compareWord(root.word, word) == SAME)
-            return root;
+        if(root ==null) {
+            return null;
+        }
+
+        if(compareWord(root.word, word) == SAME) {
+            System.out.println(word);
+            System.out.println(root.description);
+            return search(root.left, word);
+        }
+
         if(compareWord(root.word, word)  > 0)
             return search(root.right, word);
+
 
         return search(root.left, word);
     }
 
     public void deleteKey(String word){
-        deleteNode(root, word);
+        root = deleteNode(root, word) ;
+
     }
 
     public Node deleteNode(Node root, String word){
-        if(root == null)
+        if(root == null) {
+            System.out.println("Not Found");
             return null;
+        }
         int compareResult =  compareWord(root.word, word);
         if(compareResult > 0)
             root.right = deleteNode(root.right, word);
         else if(compareResult < 0)
             root.left = deleteNode(root.left, word);
         else{
+            size-=1;
+            System.out.println("Deleted Successfully");
             if(root.left == null)
                 return root.right;
 
-            else if(root.right == null)
+            else if(root.right == null) {
+                root.left = deleteNode(root.left, word);
                 return root.left;
-
+            }
             Node successor = getSuccesor(root.right);
 
             root.word = successor.word;
 
             root.right = deleteNode(root.right, successor.word);
+
+            root.left = deleteNode(root.left, word);
         }
         return root;
     }
